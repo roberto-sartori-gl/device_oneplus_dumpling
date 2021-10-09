df15408d773d0512ba9c745cb4f1d09d3fe1418a
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-10-09 19:40:25 +0200

    aosp_dumpling: remove lineage references
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- aosp_dumpling.mk
+++ aosp_dumpling.mk
@@ -21,9 +21,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dumpling device
 $(call inherit-product, device/oneplus/dumpling/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 PRODUCT_NAME := aosp_dumpling
 PRODUCT_DEVICE := dumpling
 PRODUCT_MANUFACTURER := OnePlus
